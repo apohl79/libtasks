e87172b0843e211930d10f470d72d73ae8f2a528
Andreas Pohl <apohl79@gmail.com>
2013-06-09 20:11:58 +0200

    echotest.pl: Use threads instead of fork

--- examples/echotest.pl
+++ examples/echotest.pl
@@ -1,25 +1,35 @@
 #!/usr/bin/perl
 
 use strict;
+use threads;
 use IO::Socket;
 
 $|++;
 
-my $i = 100;
-while (fork() && $i--) {}
+my $host = $ARGV[0];
+$host ||= "localhost";
+my $i = 700;
 
-my $c = IO::Socket::INET->new(Proto    => "tcp",
-			      PeerAddr => "localhost",
-			      PeerPort => "12345"
-			     ) or die "connection failed";
-$c->autoflush(1);
+my @threads;
+while ($i--) {
+    push @threads, threads->create(\&run);
+}
+foreach my $t (@threads) {
+    $t->join();
+}
 
-my $data = "echotest";
+sub run {
+    my $c = IO::Socket::INET->new(Proto    => "tcp",
+				  PeerAddr => $host,
+				  PeerPort => "12345"
+				 ) or die "connection failed";
+    $c->autoflush(1);
 
-my $t = time;
-my $count = 0;
-while (1) {
-  $c->send($data);
-  my $buf;
-  $c->recv($buf, 1024);
+    my $data = "echotest";
+    my $count = 0;
+    while (1) {
+	$c->send($data);
+	my $buf;
+	$c->recv($buf, 1024);
+    }
 }
