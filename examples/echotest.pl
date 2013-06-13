369c798f67633f3931cb1f0c3efa133dfe929003
Andreas Pohl <apohl79@gmail.com>
2013-06-13 22:10:25 +0200

    echotest.pl: Added stats

--- examples/echotest.pl
+++ examples/echotest.pl
@@ -19,20 +19,41 @@
 
 use strict;
 use threads;
+use threads::shared;
 use IO::Socket;
+use Time::HiRes qw(gettimeofday tv_interval);
 
 $|++;
 
 my $host = $ARGV[0];
 $host ||= "localhost";
-my $i = 700;
 
-my @threads;
-while ($i--) {
-    push @threads, threads->create(\&run);
+my $print_count :shared = 0;
+my $print_time :shared = 0;
+
+my $max_threads = 1000;
+my $tcount = 1;
+for (my $i = 0; $i < $tcount; $i++) {
+    threads->create(\&run);
 }
-foreach my $t (@threads) {
-    $t->join();
+
+while (1) {
+    my $interval = 5;
+    sleep $interval;
+    {
+	lock($print_count);
+	print "$tcount ".($print_count/$interval)." ".($print_time/$print_count)."\n";
+	$print_time = 0;
+	$print_count = 0;
+    }
+    if ($tcount < $max_threads) {
+	$tcount += 10;
+	for (my $i = 0; $i < 10; $i++) {
+	    threads->create(\&run);
+	}
+    } else {
+	exit;
+    }
 }
 
 sub run {
@@ -44,9 +65,25 @@ sub run {
 
     my $data = "echotest";
     my $count = 0;
+    my $time_sum = 0.0;
+    my $last_calc = time;
     while (1) {
+	my $t0 = [gettimeofday];
 	$c->send($data);
 	my $buf;
 	$c->recv($buf, 1024);
+	$time_sum += tv_interval($t0);
+	$count++;
+	if (!($count % 10)) { # check every 10 reqs
+	    my $now = time;
+	    if ($now - $last_calc >= 5) {
+		# update stats
+		lock($print_count);
+		$print_count += $count;
+		$print_time += ($time_sum/$count);
+		$time_sum = 0;
+		$count = 0;
+	    }
+	}
     }
 }
