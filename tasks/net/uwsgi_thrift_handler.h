ce5b6b58d9a5e12eab018e0c47fd3326147efe58
Andreas Pohl <apohl79@gmail.com>
2013-07-03 23:28:39 +0200

    Added thrift processor and handler

---
+++ tasks/net/uwsgi_thrift_handler.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2013 Andreas Pohl <apohl79 at gmail.com>
+ *
+ * This file is part of libtasks.
+ * 
+ * libtasks is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * libtasks is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with libtasks.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _UWSGI_THRIFT_HANDLER_H_
+#define _UWSGI_THRIFT_HANDLER_H_
+
+#include <tasks/net/uwsgi_task.h>
+
+namespace tasks {
+namespace net {
+
+template<class thrift_interface_type>
+class uwsgi_thrift_handler : public thrift_interface_type {
+public:
+	inline void set_uwsgi_task(uwsgi_task* t) {
+		m_uwsgi_task = t;
+	}
+	
+private:
+	uwsgi_task* m_uwsgi_task = nullptr;
+
+	// Provide some wrappers to access the request and response structures
+	// from a service call implementation.
+	inline uwsgi_request& request() {
+		return m_uwsgi_task->request();
+	}
+
+	inline uwsgi_request* request_p() {
+		return m_uwsgi_task->request_p();
+	}
+
+	inline uwsgi_response& response() {
+		return m_uwsgi_task->response();
+	}
+
+	inline uwsgi_response* response_p() {
+		return m_uwsgi_task->response_p();
+	}
+
+};
+
+} // net
+} // tasks
+
+#endif // _UWSGI_THRIFT_HANDLER_H_
---
+++ tasks/net/uwsgi_thrift_processor.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2013 Andreas Pohl <apohl79 at gmail.com>
+ *
+ * This file is part of libtasks.
+ * 
+ * libtasks is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * libtasks is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with libtasks.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _THRIFT_PROCESSOR_H_
+#define _THRIFT_PROCESSOR_H_
+
+#include <arpa/inet.h>
+#include <boost/shared_ptr.hpp>
+#include <protocol/TBinaryProtocol.h>
+
+#include <tasks/net/uwsgi_task.h>
+#include <tasks/net/uwsgi_thrift_transport.h>
+#include <tasks/logging.h>
+
+namespace tasks {
+namespace net {
+
+template<class processor_type, class handler_type>
+class uwsgi_thrift_processor : public tasks::net::uwsgi_task {
+public:
+	uwsgi_thrift_processor(int s) : uwsgi_task(s) {}
+	
+	virtual bool handle_request() {
+		using namespace apache::thrift::protocol;
+		using namespace apache::thrift::transport;
+		typedef uwsgi_thrift_transport<uwsgi_request> in_transport_type;
+		typedef uwsgi_thrift_transport<uwsgi_response> out_transport_type;
+		typedef TBinaryProtocol protocol_type;
+
+		boost::shared_ptr<in_transport_type> in_transport(new in_transport_type(request_p()));
+		boost::shared_ptr<out_transport_type> out_transport(new out_transport_type(response_p()));
+		boost::shared_ptr<protocol_type> in_protocol(new protocol_type(in_transport));
+		boost::shared_ptr<protocol_type> out_protocol(new protocol_type(out_transport));
+
+		// Process message
+		try {
+			boost::shared_ptr<handler_type> handler(new handler_type());
+			handler->set_uwsgi_task(this);
+			processor_type proc(handler);
+			
+			// Read a thrift object from the request, call the handler and write the respnding
+			// thrift object to the uwsgi response.
+			if (proc.process(in_protocol, out_protocol, NULL)) {
+				response().set_status("200 OK");
+			} else {
+				response().set_status("400 Bad Request");			
+			}
+		} catch (TTransportException& e) {
+			terr("TTransportException: " << e.what() << std::endl);
+			response().set_status("400 Bad Request");			
+		}
+
+		response().set_header("Content-Type", "application/x-thrift");
+		send_response();
+		
+		return true;
+	}
+};
+
+} // net
+} // tasks
+
+#endif // _THRIFT_PROCESSOR_H_
