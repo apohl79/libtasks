3926aa3d4c055ef162d467fea4f3000e0877c3bf
Andreas Pohl <apohl79@gmail.com>
2013-07-04 22:27:19 +0200

    dispatcher: Add first_worker() helper method

--- tasks/dispatcher.h
+++ tasks/dispatcher.h
@@ -29,57 +29,64 @@
 #include <tasks/ev_wrapper.h>
 
 namespace tasks {
-		
+        
 class worker;
 class task;
-	
+    
 struct signal_data;
-	
+    
 class dispatcher {
 public:
-	dispatcher();
+    dispatcher();
 
-	static std::shared_ptr<dispatcher> instance() {
-		if (nullptr == m_instance) {
-			m_instance = std::make_shared<dispatcher>();
-		}
-		return m_instance;
-	}
-		
-	// Get a free worker to promote it to the leader.
-	std::shared_ptr<worker> free_worker();
+    static std::shared_ptr<dispatcher> instance() {
+        if (nullptr == m_instance) {
+            m_instance = std::make_shared<dispatcher>();
+        }
+        return m_instance;
+    }
+        
+    // Get a free worker to promote it to the leader.
+    std::shared_ptr<worker> free_worker();
 
-	// When a worker finishes his work he returns to the free worker queue.
-	void add_free_worker(int id);
-		
-	// This method starts the system and blocks until finish() gets called. At least one task has to be passed.
-	void run(int num, task* task, ...);
+    // When a worker finishes his work he returns to the free worker queue.
+    void add_free_worker(int id);
 
-	// Terminate the workers and die.
-	void finish();
-		
+    // Returns the first worker from the workers vector. This can be useful
+    // to add tasks in situations where a worker handle is not available.
+    inline std::shared_ptr<worker> first_worker() {
+        return m_workers[0];
+    }
+        
+    // This method starts the system and blocks until finish() gets called.
+    // At least one task has to be passed.
+    void run(int num, task* task, ...);
+
+    // Terminate the workers and die.
+    void finish();
+        
 private:
-	static std::shared_ptr<dispatcher> m_instance;
-		
-	// All worker threads
-	std::vector<std::shared_ptr<worker> > m_workers;
-	int m_num_workers = 0;
+    static std::shared_ptr<dispatcher> m_instance;
+        
+    // All worker threads
+    std::vector<std::shared_ptr<worker> > m_workers;
+    int m_num_workers = 0;
 
-	// Free workers queue up here (TODO: implement lock free queue)
-	std::queue<int> m_worker_queue;
-	std::mutex m_worker_mutex;
+    // Free workers queue up here (TODO: implement lock free queue)
+    std::queue<int> m_worker_queue;
+    std::mutex m_worker_mutex;
 
-	// Condition variable/mutex used to wait for finishing up 
-	std::condition_variable m_finish_cond;
-	std::mutex m_finish_mutex;
+    // Condition variable/mutex used to wait for finishing up 
+    std::condition_variable m_finish_cond;
+    std::mutex m_finish_mutex;
 
-	ev_signal m_signal;
+    ev_signal m_signal;
 
-	// Helper to start initial tasks
-	bool start_io_task(task* task);
-	bool start_timer_task(task* task);
+    // Helper to start initial tasks
+    bool start_io_task(task* task);
+    bool start_timer_task(task* task);
 };
-	
+
 } // tasks
 
 #endif // _TASKS_DISPATCHER_H_
