fbb4bfd65e1aad454f9dd41b9df311912cf43e95
Andreas Pohl <apohl79@gmail.com>
2013-07-19 23:10:29 +0200

    buffer.h: indent

--- tasks/tools/buffer.h
+++ tasks/tools/buffer.h
@@ -35,31 +35,31 @@ public:
         setp(ptr_begin(), ptr_end());
     }
     
-	inline char* ptr_write() {
-		return pptr();
-	}
+    inline char* ptr_write() {
+        return pptr();
+    }
 
-	inline const char* ptr_write() const {
-		return pptr();
-	}
+    inline const char* ptr_write() const {
+        return pptr();
+    }
 
     inline char* ptr_read() {
-		return gptr();
-	}
+        return gptr();
+    }
 
-	inline const char* ptr_read() const {
-		return gptr();
-	}
+    inline const char* ptr_read() const {
+        return gptr();
+    }
 
-	inline char* ptr(std::size_t pos) {
-		assert(pos <= m_size);
-		return &m_buffer[pos];
-	}
+    inline char* ptr(std::size_t pos) {
+        assert(pos <= m_size);
+        return &m_buffer[pos];
+    }
 
-	inline const char* ptr(std::size_t pos) const {
-		assert(pos <= m_size);
-		return &m_buffer[pos];
-	}
+    inline const char* ptr(std::size_t pos) const {
+        assert(pos <= m_size);
+        return &m_buffer[pos];
+    }
 
     inline char* ptr_begin() {
         return &m_buffer[0];
@@ -85,68 +85,68 @@ public:
         return gptr() - ptr_begin();
     }
 
-	inline void move_ptr_write(std::size_t s) {
+    inline void move_ptr_write(std::size_t s) {
         auto owrite = offset_write();
         setp(ptr(owrite + s), ptr_end());
-	}
+    }
 
     inline void move_ptr_read(std::size_t s) {
         auto oread = offset_read();
         setg(ptr_begin(), ptr(oread + s), ptr_end());
-	}
+    }
 
-	inline void move_ptr_write_abs(std::size_t pos) {
+    inline void move_ptr_write_abs(std::size_t pos) {
         setp(ptr(pos), ptr_end());
-	}
+    }
 
-	inline void move_ptr_read_abs(std::size_t pos) {
+    inline void move_ptr_read_abs(std::size_t pos) {
         setg(ptr_begin(), ptr(pos), ptr_end());
-	}
+    }
 
-	inline std::size_t to_write() const {
-		return ptr_end() - pptr();
-	}
+    inline std::size_t to_write() const {
+        return ptr_end() - pptr();
+    }
 
-	inline std::size_t to_read() const {
-		return ptr_end() - gptr();
-	}
+    inline std::size_t to_read() const {
+        return ptr_end() - gptr();
+    }
 
-	inline std::size_t size() const {
-		return m_size;
-	}
+    inline std::size_t size() const {
+        return m_size;
+    }
 
-	inline void set_size(std::size_t s) {
+    inline void set_size(std::size_t s) {
         auto oread = offset_read();
         auto owrite = offset_write();
-		if (m_buffer.size() < s) {
-			m_buffer.resize(s + 1024);
-		}
-		m_size = s;
+        if (m_buffer.size() < s) {
+            m_buffer.resize(s + 1024);
+        }
+        m_size = s;
         setg(ptr_begin(), ptr(oread), ptr_end());
         setp(ptr(owrite), ptr_end());
- 	}
+     }
 
     inline void shrink() {
         m_buffer.resize(m_size);
     }
-	
-	inline std::size_t buffer_size() {
-		return m_buffer.size();
-	}
-	
-	inline std::streamsize write(const char_type* data, std::streamsize size) {
+    
+    inline std::size_t buffer_size() {
+        return m_buffer.size();
+    }
+    
+    inline std::streamsize write(const char_type* data, std::streamsize size) {
         return xsputn(data, size);
-	}
+    }
 
     inline std::streamsize read(char_type* data, std::streamsize size) {
         return xsgetn(data, size);
     }
-		
-	inline void clear() {
-		m_size = 0;
+        
+    inline void clear() {
+        m_size = 0;
         setg(ptr_begin(), ptr_begin(), ptr_end());
         setp(ptr_begin(), ptr_end());
-	}
+    }
 
 protected:
     // std::streambuf override
@@ -166,9 +166,9 @@ protected:
     }
 
 private:
-	std::vector<char> m_buffer;
-	std::size_t m_size = 0;
-	std::size_t m_offset = 0;
+    std::vector<char> m_buffer;
+    std::size_t m_size = 0;
+    std::size_t m_offset = 0;
 
 };
 
