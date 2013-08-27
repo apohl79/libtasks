927981ea56997ef8dfb7486a30369f870333c71d
Andreas Pohl <pohl@adtech.de>
2013-08-27 20:43:00 +0200

    io_task: Added static method to add io_tasks to the system

--- tasks/io_task.h
+++ tasks/io_task.h
@@ -28,52 +28,58 @@
 namespace tasks {
 
 class worker;
-	
+
 class io_task : public task {
 public:
-	io_task(int fd, int events);
-	virtual ~io_task();
+    io_task(int fd, int events);
+    virtual ~io_task();
 
-	inline std::string get_string() const {
-		std::ostringstream os;
-		os << "io_task(" << m_fd << ":" << m_events << ")";
-		return os.str();
-	}
+    inline std::string get_string() const {
+        std::ostringstream os;
+        os << "io_task(" << m_fd << ":" << m_events << ")";
+        return os.str();
+    }
 
-	inline int fd() const {
-		return m_fd;
-	}
+    inline int fd() const {
+        return m_fd;
+    }
 
-	inline int events() const {
-		return m_events;
-	}
+    inline int events() const {
+        return m_events;
+    }
 
-	inline ev_io* watcher() const {
-		return m_io.get();
-	}
+    inline ev_io* watcher() const {
+        return m_io.get();
+    }
 
-	inline void init_watcher() {
-		ev_io_set(m_io.get(), m_fd, m_events);
-	}
-		
-	void start_watcher(worker* worker);
-	void stop_watcher(worker* worker);
-	void update_watcher(worker* worker);
+    inline void init_watcher() {
+        ev_io_set(m_io.get(), m_fd, m_events);
+    }
 
-	virtual void dispose(worker* worker);
+    void start_watcher(worker* worker);
+    void stop_watcher(worker* worker);
+    void update_watcher(worker* worker);
+
+    virtual void dispose(worker* worker);
+
+    // This public method can be used to add io tasks outside of a worker thread
+    // context. If io tasks should be created within the context of a worker thread,
+    // you should use the protected non static method and pass a worker thread
+    // pointer.
+    static void add_io_task(io_task* task);
 
 protected:
-	void set_fd(int fd);
-	void set_events(int events);
-	void add_io_task(worker* worker, io_task* task);
-		
+    void set_fd(int fd);
+    void set_events(int events);
+    void add_io_task(worker* worker, io_task* task);
+
 private:
-	std::unique_ptr<ev_io> m_io;
-	int m_fd = -1;
-	int m_events = EV_UNDEF;
-	bool m_change_pending = false;
+    std::unique_ptr<ev_io> m_io;
+    int m_fd = -1;
+    int m_events = EV_UNDEF;
+    bool m_change_pending = false;
 };
-	
+
 } // tasks
 
 #endif // _TASKS_IO_TASK_H_
